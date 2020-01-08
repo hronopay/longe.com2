03fccd021d87c3a1f4f329e178ff5d42ea2533a7
Unknown <dedushkaegor@yahoo.com>
2020-01-08 20:02:30 +0300

    some other veryfysignup

--- www/verifysignup.php
+++ www/verifysignup.php
@@ -6,51 +6,18 @@ include_once("engine.php");
 open_connection();
 ?>
 
-<div class="general">
-
-Verify Your Email Address
-
-A verification email has been sent to nlo22-go2@yahoo.com.
-Please open the email and click on the "Verify" button to confirm that the email address belongs to you.
-
-Did not receive the email within 5 minutes?
-Make sure you provided the correct email address.
-Check your email spam/junk folder.
-
-
+'<div class="general">
 <h3>Verify Your Email Address</h3>
-
-
-
-	<div align="left">
-A verification email has been sent to nlo22-go2@yahoo.com.<br>
-Please open the email and click on the "Verify" button to confirm that the email address belongs to you.	</div>
-
-	<div align="left">
-		<strong>Did not receive the email within 5 minutes?
-Make sure you provided the correct email address.
-Check your email spam/junk folder.</strong><span class="comment">Did not receive the email within 5 minutes?
-Make sure you provided the correct email address.
-Check your email spam/junk folder.</span><br>
-		<input type="password" name="phone" maxlength="100">
+	<div align="left"><span class="verifyemail" style="font-size:14px; "><strong>
+		A verification email has been sent to <span style="color:#66FF66; "> '.$_POST['mail'].'</span>.<br>
+		Please open the email and click on the "Verify" button to confirm that the email address belongs to you.
+		<br><br>
+		Did not receive the email within 5 minutes?<br>
+		- Make sure you provided the correct email address.<br>
+		- Check your email spam/junk folder.</strong></span>
 	</div>
-		
-
-
-
-	<div id="signUpWarning" align="left">
-     
-	 <span class="comment">We are committed to complying with all applicable regulations that help prevent, detect and remediate unlawful behavior by customers and virtual asset developers when using the BuySell Project trading platform or any other BuySell Project services.</span>
-	</div>
-
-</div> 
+</div>'
 	                                      
-<script language="javascript">
-<!--
-	var data = new Date;
-	document.getElementById('id_num').value = data.getTime().toString().substr(7);
--->
-</script>
 
 <?php 
 	close_connection();
@@ -59,7 +26,3 @@ Check your email spam/junk folder.</span><br>
 // The js functions should be placed below 	 "include('include/footer.php');" instuction as the jquery libs are in footer.php now
 //-------------------------------------------------------------------------------------------------------------------------------	
 ?>
-
-<script>
-$( "#radioset" ).buttonset();
-</script>
