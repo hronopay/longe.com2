16ab96332b2aacdca50ec824d32243365aac960d
hronopay <dedushkaegor@yahoo.com>
2020-01-16 01:34:45 +0300

    table layout

--- www/exchange/market.php
+++ www/exchange/market.php
@@ -2,38 +2,123 @@
   <div id="headermarket">Market</div>
 </div>
 
-<div id="charts_instr">
-  <div id="instr"> 
-    instr<br>instr<br>
-    <div class="outer">
-      <div class="block">         
-            fvbfh<br>fvbfh<br>f
+<table width="100%"  cellspacing="2" cellpadding="1" height="350px">
+  <tr>
+
+    <td>
+	  	<div id="charts">
+	    	charts<br>charts<br>charts<br>
+  		</div>
+	  </td>
+    
+    <td width="500px">
+	    <div id="instr"> 
+        instr
+        <div class="outer">
+          <div class="block">         
+            instr<br>instr<br>instr<br>
+            instr<br>instr<br>instr<br>
+            instr<br>instr<br>instr<br>
+            instr<br>instr<br>instr<br>
+            instr<br>instr<br>instr<br>
+            instr<br>instr<br>instr<br>
+            instr<br>instr<br>instr<br>
+            instr<br>instr<br>instr<br>
+            instr<br>instr<br>instr<br>
+            instr<br>instr<br>instr<br>
+            instr<br>instr<br>instr<br>
+            instr<br>instr<br>instr<br>
+            instr<br>instr<br>instr<br>
+
+          </div>
+        </div>
       </div>
-    </div>
-    </div>
-  <div id="charts">
-    charts<br>charts<br>charts<br>
-  </div>
-    </div>
+  	</td>
+  
+  </tr>
+</table>
 
-<div id="deals_actions_orders">
-  <div id="deals"><div class="outer"><div class="block">         
-            fvbfh<br>fvbfh<br>f
-</div></div>
-</div>
-  <div id="actions"></div>
-  <div id="orders"><div class="outer"><div class="block">         
-            fvbfh<br>fvbfh<br>f
-</div></div>
-</div>
-</div>
+<table width="100%"  cellspacing="2" cellpadding="1" height="100%">
+  <tr>
+    <td>
+      <table width="100%"   cellspacing="2" cellpadding="1">
+        <tr>
+          <td>  
+            <div id="deals">
+  	          <div class="outer">	
+		            <div class="block">         
+                  deals<br>deals<br>deals<br>
+                  deals<br>deals<br>deals<br>
+                  deals<br>deals<br>deals<br>
+                  deals<br>deals<br>deals<br>
+                  deals<br>deals<br>deals<br>
+                  deals<br>deals<br>deals<br>
+                  deals<br>deals<br>deals<br>
+                  deals<br>deals<br>deals<br>
+                  deals<br>deals<br>deals<br>
+                  deals<br>deals<br>deals<br>
+                  deals<br>deals<br>deals<br>
+                  deals<br>deals<br>deals<br>
+                  deals<br>deals<br>deals<br>
+                  deals<br>deals<br>deals<br>
+                  deals<br>deals<br>deals<br>
+                  deals<br>deals<br>deals<br>
+                  deals<br>deals<br>deals<br>
+                  deals<br>deals<br>deals<br>
+                  deals<br>deals<br>deals<br>
+            		</div>
+	            </div>
+            </div>
+          </td>
+          <td width="150px">  
+            <div id="actionsBid">
+              BID
+            </div></td>
+          <td width="150px">  
+            <div id="actionsAsk">
+              ASK
+            </div>
+          </td>
+        </tr>
+      </table>
+    </td>
+    <td width="600px">
+      <table width="100%"   cellspacing="2" cellpadding="1">
+        <tr>
+          <td width="600px">  
+            <div id="orders">
+  	          <div class="outer">
+		            <div class="block">         
+                  orders<br>orders<br>orders<br>
+                  orders<br>orders<br>orders<br>
+                  orders<br>orders<br>orders<br>
+                  orders<br>orders<br>orders<br>
+                  orders<br>orders<br>orders<br>
+                  orders<br>orders<br>orders<br>
+                  orders<br>orders<br>orders<br>
+                  orders<br>orders<br>orders<br>
+                  orders<br>orders<br>orders<br>
+                  orders<br>orders<br>orders<br>
+                  orders<br>orders<br>orders<br>
+                  orders<br>orders<br>orders<br>
+                  orders<br>orders<br>orders<br>
+                  orders<br>orders<br>orders<br>
+                  orders<br>orders<br>orders<br>
+                  orders<br>orders<br>orders<br>
+                  orders<br>orders<br>orders<br>
+                  orders<br>orders<br>orders<br>
+                  orders<br>orders<br>orders<br>
+                  orders<br>orders<br>orders<br>
+                  orders<br>orders<br>orders<br>
+                  orders<br>orders<br>orders<br>
+                  orders<br>orders<br>orders<br>
+                </div>
+              </div>
+            </div>
+          </td>
+        </tr>
+      </table>	
+	  </td>
+  </tr>
+</table>
 
-
-
-
-
-
-
-
-
-<div></div>
